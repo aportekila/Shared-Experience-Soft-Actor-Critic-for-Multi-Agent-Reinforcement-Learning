01ceefd8c5b984719dd21e6fc63015bbad5755a2
Jacob <jacob.egebjerg@gmail.com>
2024-04-05 12:14:30 +0200

    Formatting and type hints

--- agent.py
+++ agent.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 
 from experience_replay import ExperienceReplay
@@ -6,12 +7,14 @@ from nets import ActorPolicyNet, CriticValueNet
 
 class ACAgent(object):
     def __init__(self, obs_shape, action_shape, capacity, device, hidden_size=64, adam_eps=1e-3, gamma=0.99,
-                 entropy_coeff=0.01, value_loss_coeff=0.5, learning_rate=3e-4, grad_clip=0.5):
+                 entropy_coeff=0.01, value_loss_coeff=0.5, learning_rate=3e-4, grad_clip=0.5, tau=5e-4, batch_size=32):
         self.device = device
+        self.batch_size = batch_size
         self.gamma = gamma
         self.entropy_coeff = entropy_coeff
         self.value_loss_coeff = value_loss_coeff
         self.grad_clip = grad_clip
+        self.tau = tau
 
         self.memory = ExperienceReplay(capacity)
 
@@ -22,17 +25,17 @@ class ACAgent(object):
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=learning_rate, eps=adam_eps)
         self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=learning_rate, eps=adam_eps)
 
-    def act(self, state):
+    def act(self, state: np.ndarray) -> np.integer:
         state = torch.FloatTensor(state).to(self.device)
         with torch.no_grad():
-            action = self.actor(state)
+            action = self.actor(state).mode()
         return action.cpu().numpy()
 
     def remember(self, state, action, reward, next_state, done):
-        self.memory.push(state, action, reward, next_state, done)
+        self.memory.push(state, action, reward, next_state, int(done))
 
-    def learn(self):
-        raise NotImplementedError("TODO: Implement the learn method")
+    def learn(self, num_steps=50):
+        raise NotImplementedError("Not implemented")
 
     def save(self, path):
         params = {
@@ -45,6 +48,11 @@ class ACAgent(object):
         params = torch.load(path, map_location=self.device)
         self.actor.load_state_dict(params['actor'])
         self.critic.load_state_dict(params['critic'])
+        self.critic_target.load_state_dict(params['critic'])
+
+    def soft_update(self):
+        for target_param, local_param in zip(self.critic_target.parameters(), self.critic.parameters()):
+            target_param.data.copy_(self.tau * local_param.data + (1.0 - self.tau) * target_param.data)
 
 
 class SEACAgent(ACAgent):
--- experience_replay.py
+++ experience_replay.py
@@ -1,25 +1,32 @@
+import numpy as np
 import torch
 from collections import deque
 import random
 
+
 class ExperienceReplay(object):
-    def __init__(self, capacity):
+    def __init__(self, capacity: int):
         self.capacity = capacity
         self.memory = deque(maxlen=capacity)
-        
-    def push(self, state, action, reward, next_state, done):
+
+    def push(self, state: np.ndarray, action: np.number, reward: np.float64, next_state: np.ndarray, done: int):
         self.memory.append((state, action, reward, next_state, done))
-        
-    def sample(self, batch_size):
+
+    def sample(self, batch_size: int) -> list[tuple[np.ndarray, np.number, np.float64, np.ndarray, int]]:
         return zip(*random.sample(self.memory, batch_size))
 
-    def sample_tensor(self, batch_size, device):
+    def sample_tensor(self, batch_size: int, device: str) -> (
+            tuple)[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         state, action, reward, next_state, done = self.sample(batch_size)
-        return torch.tensor(state, device=device), torch.tensor(action, device=device), torch.tensor(reward, device=device), torch.tensor(next_state, device=device), torch.tensor(done, device=device)
-    
-    def __len__(self):
+        return (torch.tensor(state, device=device),
+                torch.tensor(action, device=device),
+                torch.tensor(reward, device=device),
+                torch.tensor(next_state, device=device),
+                torch.tensor(done, device=device)
+                )
+
+    def __len__(self) -> int:
         return len(self.memory)
-    
-    def __repr__(self):
+
+    def __repr__(self) -> str:
         return f'ExperienceReplay(capacity={self.capacity}, len={len(self.memory)})'
-    
