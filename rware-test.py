9eb7cb2107ce54f8c59fe306ae3b6d7646a75acd
Jacob <jacob.egebjerg@gmail.com>
2024-04-10 10:06:00 +0200

    minor changes to test case

--- rware-test.py
+++ rware-test.py
@@ -14,6 +14,9 @@ env = gym.make("rware-small-4ag-v1")
 # observation space is a tuple containing spaces for each agent
 agents = [ACAgent(env.observation_space[i].shape[0], env.action_space[i].n, 5000, "cuda") for i in range(env.n_agents)]
 
+for agent in agents:
+    agent.load("weights_31000.pth")
+
 # training loop
 for i in tqdm(range(int(TOTAL_ENV_STEPS / MAX_EPISODE_LENGTH))):
     states, info = env.reset()
@@ -36,10 +39,11 @@ for i in tqdm(range(int(TOTAL_ENV_STEPS / MAX_EPISODE_LENGTH))):
         episode_length += 1
         episode_reward += np.sum(rewards)
 
-        # env.render()
+        env.render()
+    print(episode_reward)
 
     for agent in agents:
         agent.learn(num_steps=10)
         agent.memory.memory.clear()
-        if i % 1000 == 0:
-            agent.save(f"weights_{i}.pth")
+        if i % 1000 == 0 and i != 0:
+            agent.save(f"weights_{i + 31000}.pth")
