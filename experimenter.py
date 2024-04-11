4660292dde4e0be71c2566d699d4c48ad3a8a85a
Jacob <jacob.egebjerg@gmail.com>
2024-04-11 13:32:39 +0200

    fixed env.step return vals and done flag at truncation
    env.step used gym return vals, updated to gymnasium

--- experimenter.py
+++ experimenter.py
@@ -8,8 +8,6 @@ from tqdm import tqdm
 from agent import ACAgent, SEACAgent
 
 
-
-
 class Experimenter(object):
     def __init__(self, env: gym.Env, agents: list[ACAgent], save_path: str, episode_max_length: int = 500):
         self.env = env
@@ -33,13 +31,14 @@ class Experimenter(object):
         while not done:
             actions = [agent.act(states[idx], training=training) for idx, agent in enumerate(self.agents)]
             # TODO: Add truncated and terminated return vals for old gym envs
-            next_states, rewards, dones, info = self.env.step(actions)
+            next_states, rewards, terminated, truncated, info = self.env.step(actions)
+
+            done = np.all(terminated) or episode_length > self.episode_max_length
 
             if training:
                 for idx, agent in enumerate(self.agents):
-                    agent.remember(states[idx], actions[idx], rewards[idx], next_states[idx], int(dones[idx]))
+                    agent.remember(states[idx], actions[idx], rewards[idx], next_states[idx], done and 1 or 0)
 
-            done = np.all(dones) or episode_length > self.episode_max_length
             states = next_states
             episode_length += 1
             episode_reward += np.sum(rewards)
@@ -52,7 +51,6 @@ class Experimenter(object):
     def learn(self, num_steps: int = 50):
         for agent_id, agent in enumerate(self.agents):
             agent.learn(num_steps=num_steps)
-            
 
     def clear_experience(self):
         for agent in self.agents:
@@ -61,23 +59,23 @@ class Experimenter(object):
     def evaluate_policies(self, num_repetitions: int = 10, render: bool = False) -> dict:
         for agent in self.agents:
             agent.eval()
-        
+
         rewards, lengths = [], []
         for i in range(num_repetitions):
             episode_reward, episode_length = self.generate_episode(training=True, render=(i == 0 and render))
             rewards.append(episode_reward)
             lengths.append(episode_length)
-            
+
         for agent in self.agents:
             agent.train()
-            
+
         return {
             "mean_reward": np.mean(rewards),
             "std_reward": np.std(rewards),
             "mean_length": np.mean(lengths),
             "std_length": np.std(lengths)
         }
-    
+
     def run(self, args):
         num_episodes = args.total_env_steps // self.episode_max_length
         for episode in tqdm(range(num_episodes)):
@@ -90,7 +88,7 @@ class Experimenter(object):
                     print(f"Episode {episode}: {reward}")
                 self.learn(num_steps=args.num_gradient_steps)
                 self.clear_experience()
-                
+
                 if episode % args.evaluate_frequency == 0 or episode == num_episodes - 1:
                     result = self.evaluate_policies(args.evaluate_episodes, render=args.render)
                     self.experiment_history["episode"].append(episode)
@@ -100,11 +98,9 @@ class Experimenter(object):
                     print()
                     for agent_id, agent in enumerate(self.agents):
                         agent.save(f"{self.save_path}/agent_{agent_id}.pth")
-                    
+
                     # save results periodically
                     np.save(f"{self.save_path}/experiment_history.npy", self.experiment_history)
-        
-        
 
 
 implemented_agent_types = ["IAC", "SNAC", "SEAC"]
@@ -120,7 +116,7 @@ def create_experiment(args) -> Experimenter:
     se_lambda_value = args.SEAC_lambda_value
     save_path = args.save_path
     batch_size = args.batch_size
-    
+
     assert (agent_type in implemented_agent_types)
 
     env = gym.make(env_name)
@@ -134,7 +130,7 @@ def create_experiment(args) -> Experimenter:
         for i in range(num_agents):
             agent = ACAgent(env.observation_space[i].shape[0], env.action_space[i].n,
                             capacity=capacity, device=device, batch_size=batch_size)
-            
+
             agent_list.append(agent)
 
     # Several references to the same agent (shared network)
@@ -151,7 +147,7 @@ def create_experiment(args) -> Experimenter:
                               capacity=capacity, device=device,
                               agent_list=agent_list, lambda_value=se_lambda_value, batch_size=batch_size)
             agent_list.append(agent)
-            
+
     if args.pretrain_path is not None:
         for agent_id, agent in enumerate(agent_list):
             agent.load(f"{args.pretrain_path}/agent_{agent_id}.pth")
