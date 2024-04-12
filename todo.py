85c88966c565d87102c35e1c660162180217a6a6
Jacob <jacob.egebjerg@gmail.com>
2024-04-12 17:54:21 +0200

    Create todo.py

---
+++ todo.py
@@ -0,0 +1,7 @@
+# We can just commit stuff to this list, IDE has feature so you can view all todos
+# TODO Implement replay buffer so that it supports multiple episodes / agents (e.g. do n_step td calc before pushing)
+# TODO Create LBF wrapper
+# TODO Continuous action space support
+# TODO Multiwalker env
+# TODO Verify set_seed implementation
+# TODO Debug SEAC / tune parameters
