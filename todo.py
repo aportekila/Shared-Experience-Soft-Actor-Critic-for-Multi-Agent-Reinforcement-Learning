7994a282a2e39d15b482cd13dd56d92c6be23c2f
aportekila <akgula15@itu.edu.tr>
2024-04-13 16:18:57 +0200

    update todos

--- todo.py
+++ todo.py
@@ -1,7 +1,5 @@
 # We can just commit stuff to this list, IDE has feature so you can view all todos
-# TODO Implement replay buffer so that it supports multiple episodes / agents (e.g. do n_step td calc before pushing)
 # TODO Create LBF wrapper
 # TODO Continuous action space support
 # TODO Multiwalker env
-# TODO Verify set_seed implementation (currently only on initial reset)
 # TODO Debug SEAC / tune parameters
