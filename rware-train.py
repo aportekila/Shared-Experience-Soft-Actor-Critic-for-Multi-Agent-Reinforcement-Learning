eace9b025b94f1b70ca7eec8c4c2e9d9c7dddba0
Jacob <jacob.egebjerg@gmail.com>
2024-04-16 16:49:14 +0200

    Made multiwalker default

--- rware-train.py
+++ rware-train.py
@@ -5,10 +5,10 @@ import json
 
 from experimenter import create_experiment
 
-
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("--env", type=str, default="Foraging-10x10-3p-3f-v2", choices=["rware-tiny-4ag-v1", "Foraging-10x10-3p-3f-v2"])
+    argparser.add_argument("--env", type=str, default="multiwalker",
+                           choices=["multiwalker", "rware-tiny-4ag-v1", "Foraging-10x10-3p-3f-v2"])
     argparser.add_argument("--agent_type", type=str, default="SNAC", choices=["IAC", "SNAC", "SEAC", "SESAC"])
     argparser.add_argument("--episode_max_length", type=int, default=None)
     argparser.add_argument("--total_env_steps", type=int, default=50000000)
@@ -18,26 +18,26 @@ if __name__ == "__main__":
     argparser.add_argument("--seed", type=int, default=0)
     argparser.add_argument("--evaluate_frequency", type=int, default=100)
     argparser.add_argument("--evaluate_episodes", type=int, default=5)
-    
+
     argparser.add_argument("--buffer_size", type=int, default=1_000_000)
-    
-    argparser.add_argument("--update_frequency", type=int, default=2, help="Number of episodes between updates") # allows multiple episodes to be used for a single update
+
+    argparser.add_argument("--update_frequency", type=int, default=2,
+                           help="Number of episodes between updates")  # allows multiple episodes to be used for a single update
     argparser.add_argument("--num_gradient_steps", type=int, default=5)
     argparser.add_argument("--batch_size", type=int, default=128)
     argparser.add_argument("--verbose", type=int, default=1, choices=[0, 1, 2])
-    
+
     argparser.add_argument("--render", default=False, action="store_true")
-    
+
     argparser.add_argument("--n_steps", type=int, default=5)
-    
-    
+
     # SEAC related
     argparser.add_argument("--SEAC_lambda_value", type=float, default=1.0)
-    
+
     args = argparser.parse_args()
     args.device = "cuda" if torch.cuda.is_available() else "cpu"
-    args.device = "cpu" # GPU overhead is greater than speedup gains
-    
+    args.device = "cpu"  # GPU overhead is greater than speedup gains
+
     args.save_path = f"{args.save_path}/{args.env}/{args.agent_type}/{args.n_steps}/{args.seed}"
     os.makedirs(args.save_path, exist_ok=True)
     # Save arguments
@@ -46,8 +46,3 @@ if __name__ == "__main__":
 
     experimenter = create_experiment(args)
     experimenter.run(args)
-    
-    
-    
-    
-    
